3343c96dce76aebe44486808edd1d377424341bf
Willie <williewalson@gmail.com>
2018-04-09 18:29:29 -0500

    donezo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader
-(template_dir))
+(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
